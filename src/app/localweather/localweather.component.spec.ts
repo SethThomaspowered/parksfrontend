c9e8ac618222880343b5c62e1a1b4e5400264ec8
Seth Thomas <athomas.seth@gmail.com>
2022-02-09 17:39:35 -0500

    Add LocalWeather

---
+++ src/app/localweather/localweather.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LocalweatherComponent } from './localweather.component';
+
+describe('LocalweatherComponent', () => {
+  let component: LocalweatherComponent;
+  let fixture: ComponentFixture<LocalweatherComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LocalweatherComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LocalweatherComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
