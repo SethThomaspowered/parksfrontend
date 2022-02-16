96b712c369b3fd4ff8b4b545aa46988b69f97d30
Seth Thomas <athomas.seth@gmail.com>
2022-02-15 23:48:10 -0500

    Create addPark method

--- src/app/backend.service.ts
+++ src/app/backend.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 
@@ -6,5 +7,21 @@ import { environment } from 'src/environments/environment';
 })
 export class BackendService {
   uri = environment.uri;
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  getParks(){
+    return this.http.get(`${this.uri}/api/parks`)
+  }
+  addPark(name: string, address: string, city: string, county: string, state: string, zipCode: number){
+    const park = {
+      "name": name,
+      "address": address,
+      "city": city,
+      "county": county,
+      "state": state,
+      "zipCode": zipCode
+    };
+    console.log(park);
+    this.http.post(`${this.uri}/api/parks`, park).subscribe(response=> console.log(response));
+    alert("Park has been added.")
+  }
 }
